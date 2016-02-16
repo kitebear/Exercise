97eb15077e48117577d270788a9d416fa5e4f60c
dhxie <xie5721072@163.com>
2016-02-16 16:41:08 +0800

    one to one

--- 每日一练/判断javascript基本数据类型.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by xiedonghao on 16/2/15.
- */
-toString.call('nihao')
-//"[object String]"
-toString.call(123)
-//"[object Number]"
--- 每日一练/正则匹配重复字符.js
+++
@@ -1,7 +0,0 @@
-var reg = /([a-zA-Z0-9])\1+(?=\1{3})/g//TODO:完成正则
-var ret = "abbbbbeeee12222222223".replace(reg, "");
-console.log(ret);  // abbbeeee12223
-var a = "1234567890";
-var ret = a.replace(/((?=\d)\d{3})+\b/,"");
-console.log(ret);
-
